704058432a04603107cba4f3d602fa2447bbf2a0
Viren Desai <vdd@uchicago.edu>
2024-02-19 11:09:31 -0600

    bash script for setting up folder structure, and minor edits to BSM pricer

---
+++ setup_project.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Instructions:
+# 1. Save this file in your project root directory
+# 2. Navigate to the project root directory in your terminal
+# 3a. Run the following command: `bash setup_project.sh`
+# 3b. If 3a doesn't work, run the following command: `chmod +x setup_project.sh` then `bash setup_project.sh`
+
+# Define the directory structure you want to create
+folders=(
+    "data"
+    "notebooks"
+    "output"
+    "reports"
+    "src"
+    "data/raw"
+    "data/clean"
+    "data/pulled"
+    "data/manual"
+)
+
+# Create the folders
+for folder in "${folders[@]}"; do
+    mkdir -p "$folder"
+done
+
+touch README.md .gitignore .env
+
+echo "Folder structure created successfully!"
