857132c97328325386d336d164c386e43da09f75
ihammock <ihammock@uchicago.edu>
2024-02-22 17:24:37 -0600

    example table made

--- src/create_table_B1_.py
+++ src/create_table_B1_.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
 	tableB1_2012 = pd.read_parquet(Path(OUTPUT_DIR)  / "tableB1_2012.parquet")
 
-	tableB1_2020 = pd.read_parquet(Path(OUTPUT_DIR)  / "tableB1_2012.parquet")
+	tableB1_2023 = pd.read_parquet(Path(OUTPUT_DIR)  / "tableB1_2012.parquet")
 
 	step_order = ['Starting', 'Level 1 filters']
 
@@ -29,13 +29,69 @@ if __name__ == "__main__":
 	# Sort the DataFrame by the 'Step' column
 	tableB1_2012.sort_values(by='Step', inplace=True)
 	tableB1_2012 = tableB1_2012.replace({float('nan'): ''})
+
 	# Convert the DataFrame to LaTeX
 	latex_table = tableB1_2012[['Deleted', 'Remaining']].to_latex(index=True)
 
-	# Print or save the LaTeX table
-	print(latex_table)
 
-	tableString = 
+
+	tableString = r"""
+
+    \begin{tabular}{*{4}{l} *{11}{r} }
+       
+        
+         \multicolumn{4}{c}{}  & \multicolumn{3}{c}{OptionMetrics: 2012}  &  \multicolumn{1}{c}{} & 
+         \multicolumn{3}{c}{OptionMetrics: 2023}&  \multicolumn{1}{c}{}  &
+          \multicolumn{3}{c}{Total}  \\
+         \cline{5-7}
+                  
+         \cline{9-11}
+         \cline{13-15}
+         
+          &  & & & 
+          Deleted &  & Remaining & &
+          Deleted &  & Remaining & &
+          Deleted &  & Remaining 
+          \\
+
+       \hline
+
+	""" 
+	tableEnd = r'''
+
+	        \hline
+	    \end{tabular}
+	''' 
+	for step in step_order: 
+		group2012 = tableB1_2012[tableB1_2012['Step']==step]
+		group2023 = tableB1_2012[tableB1_2012['Step']==step]
+		for row in range(len(group2012)):
+			if row == 0: 
+				stepstr = step 
+			else: 
+				stepstr = ' '
+			rowname =  group2012.iloc[row].name
+
+			
+			g12_del = group2012.iloc[row]['Deleted'] 
+			g12_rem = group2012.iloc[row]['Remaining']
+
+			g23_del = group2023.iloc[row]['Deleted'] 
+			g23_rem = group2023.iloc[row]['Remaining']
+
+			gT_del = g12_del + g23_del
+			gT_rem = g12_rem + g23_rem
+			rowString = f"""
+				{stepstr} & & {rowname} & &
+				{g12_del} & & {g12_rem} & &
+				{g23_del} & & {g23_rem} & &
+				{gT_del} & & {gT_rem} {r"\\"}
+			""" 
+			tableString = tableString + rowString
+
+
+	tableString = tableString + tableEnd
+
 	path = OUTPUT_DIR / f'tableB1_2012.tex'
 	with open(path, "w") as text_file:
-	    text_file.write(latex_table)
+	    text_file.write(tableString)
