ef5f8c60b7f1cbdedbefbb43afd43262824bdb18
harrypandas <hholt@uchicago.edu>
2024-02-24 15:10:02 -0600

    Add level 2 filters and save filtered data to parquet file

--- src/filter_option_data_02.py
+++ src/filter_option_data_02.py
@@ -9,14 +9,21 @@ import config
 DATA_DIR = Path(config.DATA_DIR)
 
 def calc_time_to_maturity(df):
+    """Calculate time to maturity in days.
+    """
     ttm = (df['exdate'] - df['date']).dt.days
     return ttm
 
 def calc_time_to_maturity_yrs(df):
+    """Calculate time to maturity in years.
+    """
     ttm_yrs = calc_time_to_maturity(df) / 365
     return ttm_yrs
 
 def filter_time_to_maturity(df):
+    """Days to Maturity <7 or >180 Filter: Filter options 
+       based on time to maturity.
+    """
     # calculate time to maturity >> df.time_to_maturity
     df['time_to_maturity'] = calc_time_to_maturity(df)
 
@@ -29,9 +36,20 @@ def filter_time_to_maturity(df):
     return df
 
 def filter_iv(df):
+    """IV<5% or >100% Filter: Filter options based on implied volatility.
+    """
+    df = df.loc[((df['impl_volatility']>=0.05) 
+            & (df['impl_volatility']<=1.00))
+           | (df['impl_volatility'].isna())] 
+    
     return df
 
 def filter_moneyness(df):
+    """Moneyness <0.8 or >1.2 Filter: Filter options based on moneyness.
+       We define moneyness as the ratio of the option's strike price to 
+       the stock underlying price. Moneyness field must be defined before 
+       running this function
+    """
     # check if moneyness (mnyns) is already in the dataframe
     if 'mnyns' not in df.columns:
         # if not, calculate moneyness
@@ -41,9 +59,14 @@ def filter_moneyness(df):
     return df
 
 def filter_implied_interest_rate(df):
+    """Implied Interest Rate <0% Filter: Filter options based on implied interest rate.
+       Implied interest rate field must be defined before running this function.
+    """
     return df
 
 def apply_l2_filters(df):
+    """Apply all level 2 filters to the dataframe.
+    """
     df = filter_time_to_maturity(df)
     df = filter_iv(df)
     df = filter_moneyness(df)
@@ -51,6 +74,9 @@ def apply_l2_filters(df):
     return df
 
 if __name__ == "__main__": 
+    """Save the filtered data to a parquet file.
+       Must first load all options data, apply level 1 filters, and then apply level 2 filters.
+    """
     df = l1.load_all_optm_data()
     df_f1, df_sum, df_tableB1 = f1.appendixBfilter_level1(df)
     df_f2 = apply_l2_filters(df_f1)
