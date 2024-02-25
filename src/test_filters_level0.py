467ded6ebec38b9b555e973d1dc2f9993d03d517
harrypandas <hholt@uchicago.edu>
2024-02-25 14:38:34 -0600

    create load functionality units tests

--- src/test_filters_level0.py
+++ src/test_filters_level0.py
@@ -8,12 +8,53 @@ import load_option_data_01 as l1
 WRDS_USERNAME = Path(config.WRDS_USERNAME)
 DATA_DIR = Path(config.DATA_DIR)
 
-def test_starting_data_shape():
-    # test that the data is the same shape as the original
-    df = l1.load_all_optm_data(DATA_DIR)
-    assert df.shape[0] == 3410580
+""" Unit tests for the following:
+    1). Determing if load of data from wrds using load_all_optm_data function was pulled correctly.
+    2). Confirming option distribution is expected.
 
-def test_starting_option_type_dist():
-    # test that the option type distribution is the same as the original
+"""
+def test_load_option_data_shape():
+    """Test that the data has the correct shape after pulling from wrds using load function.
+    """
+    # call function to load data
     df = l1.load_all_optm_data(DATA_DIR)
-    assert df['cp_flag'].value_counts().to_dict() == {'P': 1706360, 'C': 1704220}
+    # expected result
+    expected = 3410580
+    # assert that the number of rows is the same as the original
+    assert df.shape[0] == expected, "The number of rows should return 3,410,580"
+
+def test_load_option_data_columns():
+    """Test that the data has the correct columns after pulling from wrds using load function.
+    """
+    # call function to load data
+    df = l1.load_all_optm_data(DATA_DIR)
+    # expected result
+    expected_columns = [
+                        'secid', 'date', 'open', 'close', 'cp_flag',
+                        'exdate', 'impl_volatility', 'tb_m3', 'volume',
+                        'open_interest', 'best_bid', 'best_offer', 'strike_price', 
+                        'contract_size'
+                        ]
+    # assert that the columns are the same as the original
+    assert all(col in df.columns for col in expected_columns)
+
+def test_load_option_data_dae_valadity():
+    """Test that the data has the correct date range after pulling from wrds using load function.
+    """
+    # call function to load data
+    df = l1.load_all_optm_data(DATA_DIR)
+    # expected result
+    expected = pd.to_datetime(['1996-01-04', '2012-01-31'])
+    # assert that the date range is the same as the original
+    assert df['date'].min() == expected[0], "The date range should start from 1996-01-01"
+    assert df['date'].max() == expected[1], "The date range should end at 2012-01-31"
+
+def test_load_option_type_dist():
+    """Test that the option type distribution is the same as the original.
+    """
+    # call function to load data
+    df = l1.load_all_optm_data(DATA_DIR)
+    # expected result 'P': 1706360, 'C': 1704220
+    expected = {'P': 1706360, 'C': 1704220}
+    # assert that the option type distribution is the same as the original
+    assert df['cp_flag'].value_counts().to_dict() == expected
